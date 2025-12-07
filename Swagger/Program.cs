dab93fa0c578253be4db5c294e2b6afb41586b25
Ganesh Kate <ganeshkate520@outlook.com>
2025-12-07 15:49:39 +0530

    Add Basic API Information

--- Swagger/Program.cs
+++ Swagger/Program.cs
@@ -1,11 +1,29 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo()
+    {
+        Title = "Swagger App",
+        Version = "v1",
+        Description = "This is a swagger app",
+        //Contact = new OpenApiContact()
+        //{
+        //    Url = new Uri("http://localhost:5000/swagger/index.html"),
+        //    Email="ganeshkate520@gmail.com",
+        //    Name= "Swagger App",
+        //}
+    });
+
+});
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -16,9 +34,13 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "V1 Test");
+        c.RoutePrefix = string.Empty;
+    });
 }
-   
+
 
 
 app.MapControllers();
