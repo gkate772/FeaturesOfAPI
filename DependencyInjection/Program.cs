7cd1b3a59068ebf4d5802403b08fdfbc27e9ffdc
Ganesh Kate <ganeshkate520@outlook.com>
2025-12-13 17:48:52 +0530

    Property Usage (Still High Coupling)

--- DependencyInjection/Program.cs
+++ DependencyInjection/Program.cs
@@ -4,7 +4,13 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        ReportService reportService = new ReportService(new SmsService(), new WhatsAppService(), new DatabaseLogger());
+        //ReportService reportService = new ReportService(new SmsService(), new WhatsAppService(), new DatabaseLogger());
+        ReportService reportService = new ReportService()
+        {
+            SmsService = new SmsService(),
+            WhatsAppService = new WhatsAppService(),
+            DatabaseLogger = new DatabaseLogger()
+        };
         reportService.GenerateReport();
     }
 }
--- DependencyInjection/WithoutDI/ReportService.cs
+++ DependencyInjection/WithoutDI/ReportService.cs
@@ -13,19 +13,25 @@ namespace DependencyInjection.WithoutDI
         //private WhatsAppService _whatsAppService = new WhatsAppService();
         //private DatabaseLogger _databaseLogger = new DatabaseLogger();
 
-        private SmsService _smsService;
-        private WhatsAppService _whatsAppService;
-        private DatabaseLogger _databaseLogger;
+        //private SmsService _smsService;
+        //private WhatsAppService _whatsAppService;
+        //private DatabaseLogger _databaseLogger;
 
-        // Constructor uses concrete classes
-        public ReportService(SmsService smsService,
-                             WhatsAppService whatsAppService,
-                             DatabaseLogger databaseLogger)
-        {
-            _smsService = smsService;
-            _whatsAppService = whatsAppService;
-            _databaseLogger = databaseLogger;
-        }
+        //// Constructor uses concrete classes
+        //public ReportService(SmsService smsService,
+        //                     WhatsAppService whatsAppService,
+        //                     DatabaseLogger databaseLogger)
+        //{
+        //    _smsService = smsService;
+        //    _whatsAppService = whatsAppService;
+        //    _databaseLogger = databaseLogger;
+        //}
+
+
+        //Property Usage
+        public SmsService SmsService { get; set; }
+        public WhatsAppService WhatsAppService { get; set; }
+        public DatabaseLogger DatabaseLogger { get; set; }
         public void GenerateReport()
         {
             Console.WriteLine("Report generated");
@@ -33,9 +39,13 @@ namespace DependencyInjection.WithoutDI
             // _logger.Log("Report generated successfully");
             // _emailService.SendEmail("Report generated successfully");
 
-            _databaseLogger.LogToDatabase("Report generated");
-            _smsService.SendSms("Report generated");
-            _whatsAppService.SendWhatsApp("Report generated");
+            //_databaseLogger.LogToDatabase("Report generated");
+            //_smsService.SendSms("Report generated");
+            //_whatsAppService.SendWhatsApp("Report generated");
+
+            DatabaseLogger.LogToDatabase("Report generated");
+            SmsService.SendSms("Report generated");
+            WhatsAppService.SendWhatsApp("Report generated");
         }
     }
 }
